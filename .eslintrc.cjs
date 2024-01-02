3578c73607f34594069aedcce1ba0bd1f00653aa
tenthree <ten.three@hotmail.com>
2024-01-02 17:35:44 +0800

    chore: update project configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,8 +4,28 @@ module.exports = {
   root: true,
   extends: [
     'plugin:vue/vue3-essential',
-    '@vue/eslint-config-standard-with-typescript',
-    // '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier'
-  ]
+    // '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  rules: {
+    'no-unused-vars': 'off',
+    'vue/no-unused-vars': [
+      'warn',
+      {
+        ignorePattern: '^_'
+      }
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_'
+      }
+    ],
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off'
+  }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,38 @@
+import { URL, fileURLToPath } from 'node:url'
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 3000
+  plugins: [
+    vue(),
+    dts({
+      include: ['./src/**/*.ts', './src/**/*.vue']
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    minify: false,
+    target: 'es2020',
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      formats: ['es', 'cjs'],
+      fileName: (format) => `index.${format === 'es' ? 'mjs' : 'js'}`
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          Vue: 'vue'
+        },
+        exports: 'named'
+      }
+    }
   }
 })
